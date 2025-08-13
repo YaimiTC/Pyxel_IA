c720c3388caa24822d68fa75789f0bcb446dcb33
Ing. Omar Crespo Carrazana <ing.omarcc92@gmail.com>
2025-08-12 22:39:10 -0400

    load fields controlled by the nomenclators

--- pyxel_import_backend/models/incoterm_import_type.py
+++ pyxel_import_backend/models/incoterm_import_type.py
@@ -10,6 +10,7 @@ class IncotermImportType(models.Model):
     import_type_id = fields.Many2one('import.type', string='Import Type', required=True, ondelete='cascade')
     active = fields.Boolean(string='Active', default=True)
 
+    # Documentation block
     has_bl = fields.Boolean(string='BL')
     has_awb = fields.Boolean(string='AWB')
     has_packing_list = fields.Boolean(string='Packing List')
@@ -20,6 +21,21 @@ class IncotermImportType(models.Model):
     has_origin_certificate = fields.Boolean(string='Origin Certificate')
     has_supplier_payment_certificate = fields.Boolean(string='Supplier Payment Certificate')
     port_or_airport = fields.Boolean(string='Port/Airport?')
+
+    # Load block
+    show_cargo_type = fields.Boolean(string='Show Cargo Type')
+    show_volume = fields.Boolean(string='Show Volume')
+    show_bulk = fields.Boolean(string='Show Bulk')
+
+    show_opening_date = fields.Boolean(string='Show Opening Date')
+    show_arrival_date = fields.Boolean(string='Show Arrival Date')
+    show_release_date = fields.Boolean(string='Show Release Date')
+    show_extraction_date = fields.Boolean(string='Show Extraction Date')
+    show_return_date = fields.Boolean(string='Show Return Date')
+
+    show_shipping_company = fields.Boolean(string='Show Shipping Company')
+    show_airline = fields.Boolean(string='Show Airline')
+    show_transit_agency = fields.Boolean(string='Show Transit Agency')
 
     _sql_constraints = [
         ('unique_combination', 'unique(incoterm_id, import_type_id)', 'This combination already exists.')
