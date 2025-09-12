3cb1e42cbd29e64c6181183d846e7e5326dc400c
Ing. Omar Crespo Carrazana <ing.omarcc92@gmail.com>
2025-09-12 15:05:37 -0400

    new OWL component for the add documents view

--- pyxel_import_website/controllers/importations_controller.py
+++ pyxel_import_website/controllers/importations_controller.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from odoo import http, _
-from odoo.http import request
+from odoo.http import request, Response
 from odoo.exceptions import ValidationError
 from .base_controller import BaseController
 import base64
 import logging
+import json
 from odoo.addons.web.controllers import report
 
 _logger = logging.getLogger(__name__)
@@ -28,3 +29,31 @@ class ImportationsController(BaseController):
             'href': '/model/imports',
         }
         return request.render("pyxel_import_website.importation_view", values)
+
+    @http.route(['/update_files/<int:record_id>'], type='http', auth='public', website=True)
+    def update_files(self, record_id, **kwargs):
+        # Recuperar el registro importation.process por su ID
+        record = request.env['importation.process'].sudo().browse(record_id)
+
+        # Renderizar la plantilla y pasar el registro
+        return request.render('pyxel_import_website.update_files_template', {
+            'record': record,
+        })
+    
+    @http.route('/upload_pdf', type='http', auth='user', methods=['POST'], csrf=True)
+    def upload_pdf(self, model, record_id, field_name, **kwargs):
+        _logger.info(f"Esto entra en la ruta de upload pdf")
+        file = request.httprequest.files.get('file')
+        if not file or not file.filename.lower().endswith('.pdf'):
+            return Response(json.dumps({'error': 'Solo se permiten archivos PDF.'}), content_type='application/json')
+
+        record = request.env[model].sudo().browse(int(record_id))
+        filename_field = f"{field_name}_filename"
+        filename = file.filename or getattr(file, 'name', False)
+        
+        record.write({
+            field_name: base64.b64encode(file.read()),
+            filename_field: filename
+            })
+
+        return Response(json.dumps({'success': True}), content_type='application/json')
